344720aa6e71e5f44c862a015371b3db4c2e78a9
Andrew Scholz <yoitsscholzy@gmail.com>
2019-05-23 11:15:21 -0400

    completed footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,9 +1,41 @@
 import React from "react"
+import styles from "../css/footer.module.css"
+import links from "../constants/links"
+import socialIcons from "../constants/social-icons"
+import { Link } from "gatsby"
 
-export default () => {
+const Footer = () => {
   return (
-    <div>
-      <h1>I'm Footer</h1>
-    </div>
+    <footer className={styles.footer}>
+      <div className={styles.links}>
+        {links.map((item, index) => {
+          return (
+            <Link key={index} to={item.path}>
+              {item.text}
+            </Link>
+          )
+        })}
+      </div>
+      <div className={styles.icons}>
+        {socialIcons.map((item, index) => {
+          return (
+            <a
+              key={index}
+              href={item.url}
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              {item.icon}
+            </a>
+          )
+        })}
+      </div>
+      <div className={styles.copyright}>
+        &copy;{new Date().getFullYear()} backroads travel company | all rights
+        reserved
+      </div>
+    </footer>
   )
 }
+
+export default Footer
