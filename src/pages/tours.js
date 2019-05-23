d673b951aecfa9473e9787323c8433d6da4f36be
Andrew Scholz <yoitsscholzy@gmail.com>
2019-05-23 16:54:18 -0400

    added title component

--- src/pages/tours.js
+++ src/pages/tours.js
@@ -1,15 +1,6 @@
 import React from "react"
 import Layout from "../components/Layout"
-import Button from "../examples/Button"
 
 export default () => {
-  return (
-    <Layout>
-      hello from tours page!!!!!
-      <div>
-        <Button big>First Button</Button>
-        <Button color="#f15025">Second Button</Button>
-      </div>
-    </Layout>
-  )
+  return <Layout>hello from tours page!!!!!</Layout>
 }
