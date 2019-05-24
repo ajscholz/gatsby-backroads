c6dd32c894297e2d5ee26c6f484af2e9ccc5990c
Andrew Scholz <yoitsscholzy@gmail.com>
2019-05-24 12:45:08 -0400

    added all background images

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,13 +1,27 @@
 import React from "react"
 import { Link } from "gatsby"
 import Layout from "../components/Layout"
+import StyledHero from "../components/StyledHero"
+import { graphql } from "gatsby"
 
-const blog = () => {
+const blog = ({ data }) => {
   return (
     <Layout>
-      hello from blog page<Link to="/">back home</Link>
+      <StyledHero img={data.blogBcg.childImageSharp.fluid} />
     </Layout>
   )
 }
+
+export const query = graphql`
+  {
+    blogBcg: file(relativePath: { eq: "blogBcg.jpeg" }) {
+      childImageSharp {
+        fluid(maxWidth: 4160, quality: 90) {
+          ...GatsbyImageSharpFluid_withWebp
+        }
+      }
+    }
+  }
+`
 
 export default blog
--- src/pages/tours.js
+++ src/pages/tours.js
@@ -1,10 +1,24 @@
 import React from "react"
 import Layout from "../components/Layout"
+import StyledHero from "../components/StyledHero"
+import { graphql } from "gatsby"
 
-export default () => {
+export default ({ data }) => {
   return (
     <Layout>
-      <div>Hello from tours</div>
+      <StyledHero img={data.defaultBcg.childImageSharp.fluid} />
     </Layout>
   )
 }
+
+export const query = graphql`
+  {
+    defaultBcg: file(relativePath: { eq: "defaultBcg.jpeg" }) {
+      childImageSharp {
+        fluid(maxWidth: 4160, quality: 90) {
+          ...GatsbyImageSharpFluid_withWebp
+        }
+      }
+    }
+  }
+`
